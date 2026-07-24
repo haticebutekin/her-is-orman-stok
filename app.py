67adcafe5f85ce6509c96d52368b7caf9d6d447a
haticebutekin <hatice-butekin@hotmail.com>
2026-07-24 15:24:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1217,7 +1217,7 @@ def indir(dosya):
     
    if __name__ == "__main__":
 
-       app.run(
-           host="0.0.0.0",
-           port=10000
+    app.run(
+        host="0.0.0.0",
+        port=10000
     )
